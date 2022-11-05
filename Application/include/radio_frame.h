426a31e146ff0642140fc3e61068e9710b4041fa
Tommy <tommy574@protonmail.ch>
2022-11-05 11:15:53 +0200

    Add 2 bytes of padding to RadioFrame_t

--- Application/include/radio_frame.h
+++ Application/include/radio_frame.h
@@ -5,7 +5,7 @@
 #include "wifi_conf.h"
 
 
-#define RADIO_FRAME_HEADER_SIZE   (6)
+#define RADIO_FRAME_HEADER_SIZE   (8)
 #define RADIO_FRAME_PAYLOAD_SIZE  (WIFI_M2M_BUFFER_SIZE - RADIO_FRAME_HEADER_SIZE)
 
 
@@ -14,6 +14,7 @@ typedef struct
     /* Header */
     uint32_t  length;
     uint8_t   sequenceId;
+    uint8_t   padding[2];
 
     /* Payload */
     char      data[RADIO_FRAME_PAYLOAD_SIZE];
