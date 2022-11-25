540f09d2abf2f4bc0cb7cc0b8230d6947445ca69
Tommy <tommy574@protonmail.ch>
2022-11-25 21:40:33 +0200

    Add Doxygen comments

--- Application/include/fpa.h
+++ Application/include/fpa.h
@@ -1,12 +1,26 @@
+/** @file */
+
 #ifndef FPA_H
 #define FPA_H
 
-#define FPA_UI8(x, m, s)  ( (uint8_t)(((uint16_t)( (uint8_t)x * m)) >> s))
-#define FPA_UI16(x, m, s) ((uint16_t)(((uint32_t)((uint16_t)x * m)) >> s))
-#define FPA_UI32(x, m, s) ((uint32_t)(((uint64_t)((uint32_t)x * m)) >> s))
 
-#define FPA_I8(x, m, s)   (  (int8_t)( ((int16_t)(  (int8_t)x * m)) >> s))
-#define FPA_I16(x, m, s)  ( (int16_t)( ((int32_t)( (int16_t)x * m)) >> s))
-#define FPA_I32(x, m, s)  ( (int32_t)( ((int64_t)( (int32_t)x * m)) >> s))
+/**@{
+ *
+ * Fixed Point Arithmetic macro.
+ *
+ * @param[in] nNumber to multiply.
+ *
+ * @param[in] t   Times to multiply.
+ *
+ * @param[in] f   Fractional bits.
+ */
+#define FPA_MUL_UI8(n, t, f)  ( (uint8_t)(((uint16_t)( (uint8_t)n * (t))) >> (f)))
+#define FPA_MUL_UI16(n, t, f) ((uint16_t)(((uint32_t)((uint16_t)n * (t))) >> (f)))
+#define FPA_MUL_UI32(n, t, f) ((uint32_t)(((uint64_t)((uint32_t)n * (t))) >> (f)))
+
+#define FPA_MUL_I8(n, t, f)   (  (int8_t)( ((int16_t)(  (int8_t)n * (t))) >> (f)))
+#define FPA_MUL_I16(n, t, f)  ( (int16_t)( ((int32_t)( (int16_t)n * (t))) >> (f)))
+#define FPA_MUL_I32(n, t, f)  ( (int32_t)( ((int64_t)( (int32_t)n * (t))) >> (f)))
+/**@}*/ 
 
 #endif /* FPA_H */
