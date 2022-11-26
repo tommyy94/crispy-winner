ba74938e6da6f39147352859f56ad9b680716a3e
Tommy <tommy574@protonmail.ch>
2022-11-26 12:12:41 +0200

    Move TC tick/us macros to header
    Add TC0_CH0_TICKS_TO_US()

--- BSP/Drivers/TC_Driver/tc_driver.h
+++ BSP/Drivers/TC_Driver/tc_driver.h
@@ -1,17 +1,68 @@
+/** @file */
+
 #ifndef TC_DRIVER
 #define TC_DRIVER
 
 #include "same70.h"
 
+/**
+ * Convert microseconds to TC0 channel 0 timer ticks.
+ *
+ * TC0 f_slck configured to 18.75MHz.
+ *
+ * \f{equation}{
+ * period = 1\ tick \div f\_slck
+ *        = 0.053us
+ * \f}
+ *
+ * Avoid division, multiply instead:
+ * \f{equation}{
+ * mul = 1 \div period = 18.867924528
+ * \f}
+ *
+ * In 32_9 format:
+ * \f{equation}{
+ * mul_s32_9 = mul \times (1 << 9)
+ *           = 9660
+ * \f}
+ *
+ * @param[in]   us
+ */
+#define TC0_CH0_US_TO_TICKS(us)   (FPA_MUL_UI32((us), 9660, 9))
 
+/**
+ * Convert microseconds to TC0 channel 0 timer ticks.
+ *
+ * TC0 f_slck configured to 18.75MHz.
+ *
+ * \f{equation}{
+ * period = 1\ tick \div f\_slck
+ *        = 0.053us
+ * \f}
+ *
+ * In 32_9 format:
+ * \f{equation}{
+ * mul_s32_9 = period \times (1 << 9)
+ *           = 27
+ * \f}
+ *
+ * @param[in]   us
+ */
+#define TC0_CH0_TICKS_TO_US(us)   (FPA_MUL_UI32((us), 27, 9))
+
+
+/**
+ * Each instance has 3 channels,
+ * totaling 12 channels in the device.
+ */
 typedef enum
-{
-    /* Each instance has 3 channels,
-     * totaling 12 channels in the device.
-     */
+{   /** TCx channel 0 */
     TC_CHANNEL_0 = 0,
+    /** TCx channel 1 */
     TC_CHANNEL_1,
+    /** TCx channel 2 */
     TC_CHANNEL_2,
+    /** TCx channel count */
     TC_CHANNEL_COUNT
 } TC_Channel_t;
 
