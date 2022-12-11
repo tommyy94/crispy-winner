d2797efb50587df25794d6f32cbc0bb8a195a85a
Tommy <tommy574@protonmail.ch>
2022-12-11 19:17:17 +0200

    Add OV5640_WriteReg() and OV5640_ReadReg()

--- BSP/Drivers/OV5640/ov5640_i2c_layer.h
+++ BSP/Drivers/OV5640/ov5640_i2c_layer.h
@@ -2,8 +2,10 @@
 
 #ifndef OV5640_I2C_LAYER_H
 #define OV5640_I2C_LAYER_H
-#include <stdbool.h>
+#include <stdint.h>
 
-bool OV5640_VerifyChip(void);
+int32_t OV5640_ReadReg(uint16_t addr, uint16_t reg, uint8_t *val, uint16_t len);
+int32_t OV5640_WriteReg(uint16_t addr, uint16_t reg, uint8_t *val, uint16_t len);
+int32_t OV5640_I2C_Stub(void);
 
 #endif /* OV5640_I2C_LAYER_H */
