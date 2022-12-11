e79fa40f5ef837457b8084864a463273e0c149b8
Tommy <tommy574@protonmail.ch>
2022-12-11 19:17:17 +0200

    Add OV5640_WriteReg() and OV5640_ReadReg()

--- BSP/Drivers/OV5640/ov5640_i2c_layer.c
+++ BSP/Drivers/OV5640/ov5640_i2c_layer.c
@@ -2,77 +2,103 @@
 
 #include "ov5640_i2c_layer.h"
 #include "twi.h"
-#include <stdio.h>
+#include "ov5640.h"
 
 
-#define OV5640_ADDR                     (0x3C)
-
-/* Register map */
-#define OV5640_CHIP_ID_HIGH_BYTE_REG    (0x300A) /* Read only */
-#define OV5640_CHIP_ID_LOW_BYTE_REG     (0x300B) /* Read only */
-
-/* Bit definitions */
-#define OV5640_CHIP_ID_HIGH_BYTE        (0x56)
-#define OV5640_CHIP_ID_LOW_BYTE         (0x40)
-
-
-
-TWI_Adapter OV5640Adap =
+TWI_Adapter OV5640_Adap =
 {
     .pInst = TWIHS0,
-    .addr  = OV5640_ADDR
 };
 
 
-static void OV5640_AssertReset(void);
-static void OV5640_ReleaseReset(void);
+/**
+ * Register read 8-bit value.
+ *
+ * @param[in]   addr  7-bit slave address.
+ *
+ * @param[in]   reg   Register to read.
+ *
+ * @param[out]  val   Pointer where register contents are read.
+ *
+ * @param[in]   len   Transfer length.
+ *
+ * @retval      ret   0 if ok.
+ */
+int32_t OV5640_ReadReg(uint16_t addr, uint16_t reg, uint8_t *val, uint16_t len)
+{
+    bool ret;
+    uint8_t seq[] =
+    {
+        (reg >> 8) & 0xFF,
+        reg & 0xFF
+    };
+
+    OV5640_Adap.addr            = addr;
+    OV5640_Adap.msgArr[0].pBuf  = seq;
+    OV5640_Adap.msgArr[0].len   = 2;
+    OV5640_Adap.msgArr[0].flags = TWI_WRITE;
+    OV5640_Adap.msgArr[1].pBuf  = val;
+    OV5640_Adap.msgArr[1].len   = len;
+    OV5640_Adap.msgArr[1].flags = TWI_READ;
+    ret = TWI_Xfer(&OV5640_Adap, 2);
+    if (ret == true)
+    {
+        return OV5640_OK;
+    }
+    else
+    {
+        return OV5640_ERROR;
+    }
+}
 
 
 /**
- * Verify OV5640 chip.
+ * Register write 8-bit value.
  *
- * @param   None.
+ * @param[in]   addr  7-bit slave address.
  *
- * @retval  ret   true if chip ok, else false.
+ * @param[in]   reg   Register to write.
+ *
+ * @param[in]   val   Value to write.
+ *
+ * @param[in]   len   Transfer length.
+ *
+ * @retval      ret   0 if ok.
  */
-bool OV5640_VerifyChip(void)
+int32_t OV5640_WriteReg(uint16_t addr, uint16_t reg, uint8_t *val, uint16_t len)
 {
-    uint32_t  status;
-    uint8_t   reg[2];
-    uint8_t   chipIdH;
-    uint8_t   chipIdL;
-    uint16_t  chipId;
-    bool      ret = false;
-
-    reg[0] = OV5640_CHIP_ID_HIGH_BYTE_REG >> 8;
-    reg[1] = OV5640_CHIP_ID_HIGH_BYTE_REG & 0xFF;
-    OV5640Adap.msgArr[0].pBuf = reg;
-    OV5640Adap.msgArr[0].len = 2;
-    OV5640Adap.msgArr[0].flags = TWI_WRITE;
-    OV5640Adap.msgArr[1].pBuf = &chipIdH;
-    OV5640Adap.msgArr[1].len = 1;
-    OV5640Adap.msgArr[1].flags = TWI_READ;
-    status = TWI_Xfer(&OV5640Adap, 2);
-
-    reg[0] = OV5640_CHIP_ID_LOW_BYTE_REG >> 8;
-    reg[1] = OV5640_CHIP_ID_LOW_BYTE_REG & 0xFF;
-    OV5640Adap.msgArr[0].pBuf = reg;
-    OV5640Adap.msgArr[0].len = 2;
-    OV5640Adap.msgArr[0].flags = TWI_WRITE;
-    OV5640Adap.msgArr[1].pBuf = &chipIdL;
-    OV5640Adap.msgArr[1].len = 1;
-    OV5640Adap.msgArr[1].flags = TWI_READ;
-    status &= TWI_Xfer(&OV5640Adap, 2);
-    if (status)
+    bool ret;
+    uint8_t seq[] =
     {
-        /* Should be 0x5640 */
-        chipId = (chipIdH << 8) | chipIdL;
-        printf("OV5640 chip ID = 0x%04X\r\n", chipId);
-        if (chipId == 0x5640)
-        {
-            ret = true;
-        }
-    }
+        (reg >> 8) & 0xFF,
+        reg & 0xFF,
+        *val
+    };
 
-    return ret;
+    OV5640_Adap.addr            = addr;
+    OV5640_Adap.msgArr[0].pBuf  = seq;
+    OV5640_Adap.msgArr[0].len   = 2 + len;
+    OV5640_Adap.msgArr[0].flags = TWI_WRITE;
+    ret = TWI_Xfer(&OV5640_Adap, 1);
+    if (ret == true)
+    {
+        return OV5640_OK;
+    }
+    else
+    {
+        return OV5640_ERROR;
+    }
+}
+
+
+/**
+ * Don't allow this module to initialize/deinitialize
+ * I2C bus as it is multi-slave bus. I2C initialization
+ * handled elsewhere.
+ *
+ * @retval      return OV5640_OK
+ */
+int32_t OV5640_I2C_Stub(void)
+{
+    return OV5640_OK;
 }
