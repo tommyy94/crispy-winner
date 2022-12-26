077e7193645fcf5a619b14f427d17c24bd8712e3
Tommy <tommy574@protonmail.ch>
2022-12-26 13:03:05 +0200

    Refactor sdramc_driver

--- BSP/Drivers/SDRAMC_Driver/sdramc_driver.h
+++ BSP/Drivers/SDRAMC_Driver/sdramc_driver.h
@@ -3,32 +3,38 @@
 #ifndef SDRAMC_DRIVER_H
 #define SDRAMC_DRIVER_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 
 typedef struct
 {
-    uint32_t columnBits;                     // Number of Column Bits
-    uint32_t rowBits;                        // Number of Row Bits
-    uint32_t nb;                             // Number of Banks
-    uint32_t dataBusWidth;                   // Data Bus Width
-    uint32_t cas;                            // CAS Latency (tCAC)
-    uint32_t writeRecoveryDelay;             // Write Recovery Delay (tDPL / tWR)
-    uint32_t rowCycleDelay_RowRefreshCycle;  // Row Cycle Delay and Row Refresh Cycle (tRC & tRFC)
-    uint32_t rowPrechargeDelay;              // Row Precharge Delay (tRP)
-    uint32_t rowColumnDelay;                 // Row to Column Delay (tRCD)
-    uint32_t activePrechargeDelay;           // Active to Precharge Delay (tRAS)
-    uint32_t exitSelfRefreshActiveDelay;     // Exit Self Refresh to Active Delay (tXSR / tXSRD / tSRFX)
-    uint32_t bk1;                            // bk1 addr
-} SDRAMC_Config_t;
+    bool     isLpSdr;         /**< Low Power SDRAM                    */
+    uint32_t size;            /**< Size in bytes                      */
 
-typedef struct
-{
-    SDRAMC_Config_t cfg;
+    /* Parameters                                                     */
+    uint32_t columnBits;      /**< Number of Column Bits              */
+    uint32_t rowBits;         /**< Number of Row Bits                 */
+    uint32_t banks;           /**< Number of Banks                    */
+    uint32_t dataBusWidth;    /**< Data Bus Width                     */
+    uint32_t cas;             /**< CAS Latency (tCAS)                 */
 
-} SDRAMC_Memory_t;
+    /* Timing parameters                                              */
+    struct
+    {
+        uint32_t twr;         /**< Write Recovery Delay               */
+        uint32_t trc;         /**< Row Cycle Delay                    */
+        uint32_t trfc;        /**< Row Refresh Cycle                  */
+        uint32_t trp;         /**< Row Precharge Delay                */
+        uint32_t trcd;        /**< Row to Column Delay                */
+        uint32_t tras;        /**< Active to Precharge Delay          */
+        uint32_t txsr;        /**< Exit Self Refresh to Active Delay  */
+        uint32_t tmrd;        /**< Load Mode Register Command to
+                               *   Active or Refresh Command          */
+    } timings;
+} SDRAMC_Descriptor_t;
 
-#define SDRAMC_DBUS_WIDTH_16    (SDRAMC_CR_DBW) /* Only 16-bit support */
 
-
-void SDRAMC_Init(SDRAMC_Memory_t *pMem, uint32_t clkFreq);
+int32_t SDRAMC_Init(SDRAMC_Descriptor_t *pDesc, uint32_t clkFreq);
 
 #endif /* SDRAMC_DRIVER_H */
