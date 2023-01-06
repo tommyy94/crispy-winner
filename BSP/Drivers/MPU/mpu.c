aa563ada36d8d857ca1e499479560ea3eefaf784
Tommy <tommy574@protonmail.ch>
2023-01-06 15:50:58 +0200

    Use magic numbers instead of importing linker symbols

--- BSP/Drivers/MPU/mpu.c
+++ BSP/Drivers/MPU/mpu.c
@@ -7,31 +7,29 @@
 #define MPU_ZONE_CNT    (16u)
 
 
-extern unsigned char __FLASH_segment_start__[];
-extern unsigned char __RAM_segment_start__[];
-extern unsigned char __PER_segment_start__[];
-extern unsigned char __SDRAM_segment_start__[];
-
-
 bool MPU_Config(void)
 {
     bool              ret                  = false;
     ARM_MPU_Region_t  regTbl[MPU_ZONE_CNT] =
     {
         {
-            ARM_MPU_RBAR(0, (uint32_t)&__FLASH_segment_start__),
+            /* Flash */
+            ARM_MPU_RBAR(0, 0x00400000),
             ARM_MPU_RASR_EX(false, ARM_MPU_AP_RO, ARM_MPU_ACCESS_NORMAL(ARM_MPU_CACHEP_WB_WRA, ARM_MPU_CACHEP_WB_WRA, false), false, ARM_MPU_REGION_SIZE_1MB)
         },
         {
-            ARM_MPU_RBAR(1, (uint32_t)&__RAM_segment_start__),
+            /* SRAM */
+            ARM_MPU_RBAR(1, 0x20400000),
             ARM_MPU_RASR_EX(true, ARM_MPU_AP_FULL, ARM_MPU_ACCESS_NORMAL(ARM_MPU_CACHEP_WB_WRA, ARM_MPU_CACHEP_WB_WRA, false), false, ARM_MPU_REGION_SIZE_256KB)
         },
         {
-            ARM_MPU_RBAR(2, (uint32_t)&__PER_segment_start__),
+            /* Peripherals */
+            ARM_MPU_RBAR(2, 0x40000000),
             ARM_MPU_RASR_EX(true, ARM_MPU_AP_FULL, ARM_MPU_ACCESS_DEVICE(true), false, ARM_MPU_REGION_SIZE_512MB)
         },
         {
-            ARM_MPU_RBAR(3, (uint32_t)&__SDRAM_segment_start__),
+            /* SDRAM */
+            ARM_MPU_RBAR(3, 0x70000000),
             ARM_MPU_RASR_EX(true, ARM_MPU_AP_FULL, ARM_MPU_ACCESS_NORMAL(ARM_MPU_CACHEP_NOCACHE, ARM_MPU_CACHEP_NOCACHE, true), false, ARM_MPU_REGION_SIZE_2MB)
         }
     };
